d212db204cb98346b85d396c9576b45ecaa52407
zicunren <zicunren@163.com>
2016-10-21 12:07:15 +0800

    xxx

--- vue-simple/src/main.js
+++ vue-simple/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// new Vue({
+//   el: 'body',
+//  // render: h => h(App)
+// components:{App}
+// })
+
 new Vue({
-  el: 'body',
- // render: h => h(App)
-components:{App}
-})
+    el:'body',
+    data: {
+        message:'hello vue.js.'
+    }
+});
