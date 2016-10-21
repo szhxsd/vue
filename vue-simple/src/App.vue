c0db0fb8266d42d6288a044a4702010a251297a1
zicunren <zicunren@163.com>
2016-10-21 08:56:43 +0800

    文件名整理

---
+++ vue-simple/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+<div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <h1>{{ title }}</h1> -->
+
+    <h1 v-html="title" v-on:click='toggleFinish("hxsd")'></h1>
+
+
+</div>
+</template>
+
+<script>
+
+export default {
+    //name: 'app',
+
+     data:function(){
+        return {
+          //msg: 'Welcome to Your Vue.js App'
+          title:"<span>?</span>我是一条标题"
+        }
+    },
+
+    // data() {
+    //     return {
+    //         msg: '扔出一条信息'
+    //     }
+    // },
+
+    // components: {
+    //     Hello
+    // }
+
+    methods:{
+        toggleFinish:function(a){
+            console.log(a)
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
---
+++ webpack-simple-demo/README.md
@@ -0,0 +1,32 @@
+# demo
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+$ npm install -g cnpm --registry=https://registry.npm.taobao.org
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+``` bash
+├─dist			# 发布目录
+│   ├─.gitkeep       
+├─src			# 开发目录
+│   ├─App.vue		# App.vue组件
+│   ├─main.js		# 预编译入口
+├─.gitignore
+├─.babelrc		# babel配置文件
+├─index.html		# 主页
+├─package.json		# 项目配置文件
+├─README.md  
+├─webpack.config.js	 # webpack配置文件
+
+```
